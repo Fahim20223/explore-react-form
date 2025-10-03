c894b37d2aa4dcc8f1dc169f01dfe2e40039b40e
Fahim2023 <fahimshahrier2023@gmail.com>
2025-10-04 00:26:52 +0600

    added unctrolled field's

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import ControlledField from "./Components/ControlledField/ControlledField";
 import FormAction from "./Components/FormAction/FormAction";
 import SimpleForm from "./Components/SimpleForm/SimpleForm";
+import UnControlledField from "./Components/UnControlledField/UnControlledField";
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       {/* <SimpleForm></SimpleForm>
        */}
       {/* <FormAction></FormAction> */}
-      <ControlledField></ControlledField>
+      {/* <ControlledField></ControlledField> */}
+      <UnControlledField></UnControlledField>
     </>
   );
 }
--- src/Components/ControlledField/ControlledField.jsx
+++ src/Components/ControlledField/ControlledField.jsx
@@ -4,11 +4,25 @@ const ControlledField = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("submitted");
+
+    // if (password.length < 6) {
+    //   setError("Password must be greater than 6 words");
+    // } else {
+    //   setError("");
+    // }
   };
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
   const handlePasswordOnChange = (e) => {
     console.log(e.target.value);
     setPassword(e.target.value);
+
+    if (password.length < 6) {
+      setError("Password must be greater than 6 words");
+    } else {
+      setError("");
+    }
   };
   return (
     <div>
@@ -27,6 +41,9 @@ const ControlledField = () => {
         <br />
         <input type="submit" value="Submit" />
       </form>
+      <p style={{ color: "red" }}>
+        <small>{error}</small>
+      </p>
     </div>
   );
 };
