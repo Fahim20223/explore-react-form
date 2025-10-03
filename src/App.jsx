23089fae09490efcade95d856f8eec5b13f5f23a
Fahim2023 <fahimshahrier2023@gmail.com>
2025-10-03 17:29:25 +0600

    added react form

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import FormAction from "./Components/FormAction/FormAction";
+import SimpleForm from "./Components/SimpleForm/SimpleForm";
+
+function App() {
+  return (
+    <>
+      <h1>Explore React Form</h1>
+      {/* <SimpleForm></SimpleForm>
+       */}
+      <FormAction></FormAction>
+    </>
+  );
+}
+
+export default App;
